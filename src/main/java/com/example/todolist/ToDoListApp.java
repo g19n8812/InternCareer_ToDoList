8e881e783eaeedaf0647fda82c626da06984922c
pntshanga023a <pntshanga023a@student.wethinkcode.co.za>
2024-02-14 13:02:22 +0200

    added imports that i will be needing an a main method

---
+++ src/main/java/com/example/todolist/ToDoListApp.java
@@ -0,0 +1,15 @@
+package com.example.todolist;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ToDoListApp extends JFrame {
+
+    public static void main(String[] args) {
+
+    }
+
+}
